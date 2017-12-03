7a52e772a636afeb7d4e41459fdd2e0d37207a0a
Mitchel Hall <scubaphoto@gmail.com>
2017-12-02 17:22:21 -0800

    replaced for loops with for each functions

--- ariel-mitch/scripts/article.js
+++ ariel-mitch/scripts/article.js
@@ -55,10 +55,18 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+// for(let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+// }
 
-for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+rawData.forEach(function(post) {
+  articles.push(new Article(post));
+});
+
+// for(let i = 0; i < articles.length; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
+
+articles.forEach(function(write) {
+  $('#articles').append(write.toHtml());
+});
