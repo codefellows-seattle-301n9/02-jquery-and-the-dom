8f47bde609ed5ed6b9f3cb5b14a6c08d32192822
Mitchel Hall <scubaphoto@gmail.com>
2017-12-02 16:26:52 -0800

    Slight correction to constructor function, linked javascript and jquery into index.html

--- ariel-mitch/scripts/article.js
+++ ariel-mitch/scripts/article.js
@@ -8,12 +8,12 @@ let articles = [];
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
-  this.title = title,
-  this.category = category;
-  this.author = author;
-  this.authorUrl = authorUrl;
-  this.publishedOn = publishedOn;
-  this.body = body;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
